8aa3f659adf4a94de2eec5cb239b2284f645ac3f
rustleupchef <rustle.up.chef@gmail.com>
2026-01-26 17:26:43 -0600

    added basic backend logic

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import nodemailer from "nodemailer"
 import "dotenv/config"
 import fs from "fs";
+import http from "http";
+
+const invitationStr = fs.readFileSync("html/invitation.html", "utf-8");
 
 const transporter = nodemailer.createTransport({
     service: "gmail",
@@ -10,12 +13,38 @@ const transporter = nodemailer.createTransport({
     }
 });
 
-const htmlStr = fs.readFileSync("html/formated_invitation.html", "utf-8");
-console.log(htmlStr);
+const server = http.createServer((req, res) => {
+    if (req.url === "/head") {
+        res.writeHead(200);
+        res.end();
+    }
+    else if (req.url === "/invite" && req.method === 'POST') {
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
 
-await transporter.sendMail({
-    from: "wais.cshs@gmail.com",
-    to: "shettsid000@mysbisd.org",
-    subject: "Invitation to CSHS",
-    html: htmlStr,
+        req.on('end', () => {
+            try {
+                const data = JSON.parse(body);
+                transporter.sendMail({
+                    from: "wais.cshs@gmail.com",
+                    to: data.people,
+                    subject: "Invitation to CSHS",
+                    html: invitationStr,
+                });
+
+            } catch (error) {
+                res.writeHead(400, {"Content-Type" : "application/json"});
+                res.end(JSON.stringify({status: "error", message: error}));
+            }
+        });
+    } else {
+        res.writeHead(404, {"Content-Type": "application/json"});
+        res.end(JSON.stringify({status: "Page Not Found", message: "The page you are looking for could not be found"}));
+    }
+});
+
+server.listen(3000, () => {
+    console.log("Server is listening on port 3000");
 });
