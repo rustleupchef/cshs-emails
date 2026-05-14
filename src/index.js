3387bff39e0845f02157340f94b088fcb6cf6c32
rustleupchef <rustle.up.chef@gmail.com>
2026-05-14 14:09:23 -0500

    made the file detection more dynamic

--- src/index.js
+++ src/index.js
@@ -16,7 +16,14 @@ export default {
 		const method = request.method;
 		const path = new URL(request.url).pathname;
 
-		if (method === "POST" && path === "/invite") {
+		const map = new Map();
+		map.set("/invite", {path: "join", subject: "CSHS Invitation"});
+
+		if (method === "POST") {
+
+			if (!map.has(path)) {
+				return new Response("Invalid Path");
+			}
 			
 			const contentType = request.headers.get("content-type");
 			if (contentType && contentType.includes("application/json")) {
@@ -26,13 +33,13 @@ export default {
 						return new Response("Invalid Key");
 					}
 
-					const response = await fetch("https://raw.githubusercontent.com/WAIScshs/resource/refs/heads/main/html-emails/join.html");
+					const response = await fetch(`https://raw.githubusercontent.com/WAIScshs/resource/refs/heads/main/html-emails/${map.get(path).path}.html`);
 					const text = await response.text();
 
 					const mailOptions = {
 						from: 'wais.cshs@gmail.com',
 						to: payload.emails,
-						subject: "CSHS Invitation",
+						subject: map.get(path).subject,
 						html: text
 					};
 
